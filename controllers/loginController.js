7d36e61241cffaef4db202b01c6b0ff3b54ea9c2
KasunAb <kasunssckinfo@gmail.com>
2022-07-05 09:37:28 +0530

    Add role to response login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -13,22 +13,41 @@ const jwtSecrete = '231e07bf113b22fabaca321b96e015aea7e853d91de217d264891f54a3fb
 exports.login = async (req, res, next) => {
     const {
         username,
-        password
+        password,
+        role
     } = req.body
     // Check if username and password is provided
-    console.log(username, password)
-    if (!username || !password) {
+    //console.log(username, password)
+    if (!username || !password ||!role) {
         return res.status(400).json({
-            message: "Username or Password or Role not present",
+            message: "Username Password or Role not present",
             isLogged: false
         })
     }
 
     const vars = [username]
-    const sql = userQuary.findUser()
-    const result = await dbConnection.findExecution(sql, vars)
-
-    //console.log(result)
+    
+    switch (role) {
+        case "ADMIN":
+            roleSql = adminQuary.findAdminByUserName()
+            break;
+        case "MANAGER":
+            roleSql = managerQuary.findManagerByUserName()
+            break;
+        case "STOREKEEPER":
+            roleSql = storekeeperQuary.findStorekeeperByUsername()
+            break;
+        case "ASSISTANT":
+            roleSql = assistantQuary.findAssistantByUsername()
+            break;
+        default:
+            res.status(400).json({
+                message: "Wrong role type",
+            });
+            return
+    }
+    const result = await dbConnection.findExecution(roleSql,vars)
+    // console.log(result)
     if (result.status != 200) {
         res.status(result.status).json({
             message: "Login not successful"
@@ -41,12 +60,17 @@ exports.login = async (req, res, next) => {
         })
         return
     }
-    console.log(result.result[0].User_ID)
-    const userId = result.result[0].User_ID
-    const userRole = result.result[0].Role
+    const sql = userQuary.findUser()
+    const userResult = await dbConnection.findExecution(sql, vars)
+
+    // console.log(userResult)
+    // console.log(userResult.result[0].User_ID)
+    const userId = userResult.result[0].User_ID
+    const userRole = userResult.result[0].Role
     try {
-        bcrypt.compare(password, result.result[0].Password).then((result) => {
+        bcrypt.compare(password, userResult.result[0].Password).then((result) => {
             if (result) {
+
                 const maxAge = 3 * 60 * 60;
                 const token = jwt.sign({
                         id: userId,
@@ -63,10 +87,12 @@ exports.login = async (req, res, next) => {
                 });
                 res.status(200).json({
                     message: "User successfully Logged in",
-                    user: userId
+                    user: userId,
+                    role:role
                 });
 
             } else {
+                
                 res.status(400).json({
                     message: "Login not succesful",
                     user: userId
@@ -74,6 +100,7 @@ exports.login = async (req, res, next) => {
             }
         })
     } catch (error) {
+        
         console.log(error);
         res.status(500).json({
             message: "An error occurred"
